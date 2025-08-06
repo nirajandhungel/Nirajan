fd7c8cfba3239fd1cde19760472639ef1ac8d947
nirajandhungel <subashdhungel555@gmail.com>
2025-08-06 20:17:50 +0545

    Improved sitemap with lastmod for faster Google crawl

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://nirajandhungel.com.np',  // Replace with your actual domain
-  generateRobotsTxt: true,            // (optional) generate robots.txt file
+  siteUrl: 'https://nirajandhungel.com.np',
+  generateRobotsTxt: true,
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
+  generateIndexSitemap: true,
+  autoLastmod: true,
 };
+
