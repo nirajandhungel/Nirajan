b126da4268c8fa4e08606439cc364ac3adcfeb2b
nirajandhungel <subashdhungel555@gmail.com>
2025-08-28 21:17:00 +0545

    chore(blogs): clean up blogs

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -17,7 +17,6 @@ export default function Navbar() {
     { href: '/', label: 'Home' },
     { href: '/about', label: 'About' },
     { href: '/projects', label: 'Projects' },
-    { href: '/blogs', label: 'Blogs' },
     { href: '/contact', label: 'Contact' },
   ];
 
