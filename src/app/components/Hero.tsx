6ae0900e8d43901c682f8780a4492b51018b406e
nirajandhungel <subashdhungel555@gmail.com>
2025-08-28 21:16:06 +0545

    refactor(hero section): refine design with shadows and rounded corners

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,29 +1,43 @@
-'use client'
+"use client";
 
-import Link from 'next/link';
-import Image from 'next/image';
-import { FaGithub, FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from 'react-icons/fa';
-import { motion } from 'framer-motion';
-import { fadeInUp, fadeIn, scaleIn } from '@/utils/animations';
+import Link from "next/link";
+import Image from "next/image";
+import {
+  FaGithub,
+  FaInstagram,
+  FaLinkedin,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa";
+import { motion } from "framer-motion";
+import { fadeInUp, fadeIn, scaleIn } from "@/utils/animations";
 
 export default function Hero() {
   return (
-    <section className="py-28">
+    <section className="py-28 min-h-screen">
       <div className="container max-w-7xl mx-auto px-4">
         <div className="max-w-3xl mx-auto text-center">
-          <motion.div 
-            className='flex justify-center items-center mb-4'
+          <motion.div
+            className="flex justify-center items-center mb-4"
             {...scaleIn}
             transition={{ delay: 0.2 }}
           >
-            <Image src="/profile.png" priority={true} alt="Profile Image" width={100} height={100} className="rounded-full mb-4 w-32 h-32 object-cover ring-2 ring-primary" />
+            <Image
+              src="/profile.png"
+              priority={true}
+              alt="Profile Image"
+              width={100}
+              height={100}
+              className="rounded-full mb-4 w-32 h-32 object-cover ring-2 ring-primary"
+            />
           </motion.div>
-          <motion.h1 
+          <motion.h1
             className="text-4xl md:text-6xl font-bold mb-6"
             {...fadeInUp}
             transition={{ delay: 0.3 }}
           >
-            Hi, I&apos;m <motion.span 
+            Hi, I&apos;m{" "}
+            <motion.span
               className="text-primary"
               {...fadeIn}
               transition={{ delay: 0.8 }}
@@ -31,14 +45,14 @@ export default function Hero() {
               Nirajan Dhungel
             </motion.span>
           </motion.h1>
-          <motion.p 
+          <motion.p
             className="text-xl md:text-2xl text-gray-600 dark:text-gray-300 mb-8"
             {...fadeInUp}
             transition={{ delay: 0.4 }}
           >
-            Full Stack Developer | MERN | Web Products Developer
+            Full Stack | MERN | Web Products Developer
           </motion.p>
-          <motion.div 
+          <motion.div
             className="flex justify-center space-x-4 mb-8"
             {...fadeInUp}
             transition={{ delay: 0.5 }}
@@ -47,7 +61,7 @@ export default function Hero() {
               href="https://github.com/nirajandhungel"
               target="_blank"
               rel="noopener noreferrer"
-               aria-label="Visit Nirajan Dhungel's GitHub profile"
+              aria-label="Visit Nirajan Dhungel's GitHub profile"
               className="text-2xl text-gray-600 hover:text-icon dark:text-gray-300 dark:hover:text-icon transition-colors"
               whileHover={{ scale: 1.2 }}
               whileTap={{ scale: 0.9 }}
@@ -81,7 +95,7 @@ export default function Hero() {
               href="https://x.com/SubashDhungel18"
               target="_blank"
               rel="noopener noreferrer"
-               aria-label="Visit Nirajan Dhungel's Twitter / X profile"
+              aria-label="Visit Nirajan Dhungel's Twitter / X profile"
               className="text-2xl text-gray-600 hover:text-icon dark:text-gray-300 dark:hover:text-icon transition-colors"
               whileHover={{ scale: 1.2 }}
               whileTap={{ scale: 0.9 }}
@@ -92,7 +106,7 @@ export default function Hero() {
               href="https://www.youtube.com/@nirajan.dhungel"
               target="_blank"
               rel="noopener noreferrer"
-               aria-label="Visit Nirajan Dhungel's Youtube Channel"
+              aria-label="Visit Nirajan Dhungel's Youtube Channel"
               className="text-2xl text-gray-600 hover:text-icon dark:text-gray-300 dark:hover:text-icon transition-colors"
               whileHover={{ scale: 1.2 }}
               whileTap={{ scale: 0.9 }}
@@ -100,26 +114,22 @@ export default function Hero() {
               <FaYoutube />
             </motion.a>
           </motion.div>
-          <motion.div 
+          <motion.div
             className="flex flex-col md:flex-row justify-center gap-4"
             {...fadeInUp}
             transition={{ delay: 0.6 }}
           >
-            <motion.div
-              whileHover={{ scale: 1.05 }}
-              whileTap={{ scale: 0.95 }}
-            >
+            <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
               <Link
-                href="/nirajan_cv.pdf " target="_blank" download
+                href="/nirajan_cv.pdf "
+                target="_blank"
+                download
                 className="bg-primary inline-block w-full md:w-auto text-white px-8 py-3 rounded-lg hover:bg-primary/90 transition-colors"
               >
                 Download CV
               </Link>
             </motion.div>
-            <motion.div
-              whileHover={{ scale: 1.05 }}
-              whileTap={{ scale: 0.95 }}
-            >
+            <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
               <Link
                 href="/contact"
                 className=" inline-block w-full bg-gray-300  md:w-auto text-gray-800 dark:text-gray-800 px-8 py-3 rounded-lg hover:bg-gray-300 dark:hover:bg-gray-500 transition-colors"
@@ -132,4 +142,4 @@ export default function Hero() {
       </div>
     </section>
   );
-} 
+}
